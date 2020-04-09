b97cedc45440b07c37d7f17b263cee4bd1e67d68
ZavbiA <anja.zavbi@gmail.com>
2020-04-09 07:08:24 +0200

    nov test

---
+++ simulations/sim_kor_velika.R
@@ -0,0 +1,94 @@
+###########################################################################
+###########################################################################
+###                                                                     ###
+###               PODATKI, SIMULACIJE IN LASTNOSTI TESTOV               ###
+###                                                                     ###
+###########################################################################
+###########################################################################
+
+source("lib.r")
+source("fun.r")
+
+n <- 100 # velikost vzorca
+N <- 1000 # Število simulacij za generiranje porazdelitev
+m <- 500 # Število simulacij za vsak test
+
+##----------------------------------------------------------------
+##                         KORELACIJA: 0.9                        --
+##----------------------------------------------------------------
+
+## TESTI ZA NORMALNE PODATKE
+mu1<-0; mu2<-0; ro<-0.9; b1<-6; b2<-2
+
+podatki <- get.data(n, mu1, mu2, ro, b1, b2)
+
+test1.kor09 <- testiraj(podatki, perm.cols=c("X1","X2"), m.type="razlika", n=N)
+test2.kor09 <- testiraj(podatki, perm.cols=c("y"), m.type="razlika", n=N)
+test3.kor09 <- testiraj(podatki, perm.cols=c("X1","X2"), m.type="razmerje", n=N)
+test4.kor09 <- testiraj(podatki, perm.cols=c("y"), m.type="razmerje", n=N)
+
+
+##----------------------------------------------------------------
+##                 Velikost testa NORMAL                        --
+##----------------------------------------------------------------
+
+# Podatki pod H0
+mu1 <- 0; mu2 <-0; ro <-0.9; b1 <- 3; b2 <- 3
+
+# Test 1
+pvred1 <- replicate(m,testiraj(get.data(n, mu1, mu2, ro, b1, b2),
+                               perm.cols=c("X1","X2"), m.type="razlika", n=N)$p)
+vel1 <- round(sum(pvred1<0.05)/m,3)
+
+# Test 2
+pvred2 <- replicate(m,testiraj(get.data(n, mu1, mu2, ro, b1, b2),
+                               perm.cols=c("y"), m.type="razlika", n=N)$p)
+vel2 <- round(sum(pvred2<0.05)/m,3)
+
+# Test 3
+pvred3 <- replicate(m,testiraj(get.data(n, mu1, mu2, ro, b1, b2),
+                               perm.cols=c("X1","X2"), m.type="razmerje", n=N)$p)
+vel3 <- round(sum(pvred3<0.05)/m,3)
+
+# Test 4
+pvred4 <- replicate(m,testiraj(get.data(n, mu1, mu2, ro, b1, b2),
+                               perm.cols=c("y"), m.type="razmerje", n=N)$p)
+vel4 <- round(sum(pvred4<0.05)/m,3)
+
+velikosti.kor09 <- data.frame("Velikost" = c(vel1,vel2,vel3,vel4),
+                             row.names = paste0("Test ",1:4))
+
+##---------------------------------------------------------------
+##                    Moč testa  NORMAL                        --
+##---------------------------------------------------------------
+
+# Podatki pod H1 (isti kot prej)
+mu1 <- 0; mu2 <-0; ro <-0.9; b1 <- 6; b2 <- 2
+
+# Test 1
+pvred1 <- replicate(m,testiraj(get.data(n, mu1, mu2, ro, b1, b2),
+                               perm.cols=c("X1","X2"), m.type="razlika", n=N)$p)
+moc1 <- round(sum(pvred1<0.05)/m,3)
+
+# Test 2
+pvred2 <- replicate(m,testiraj(get.data(n, mu1, mu2, ro, b1, b2),
+                               perm.cols=c("y"), m.type="razlika", n=N)$p)
+moc2 <- round(sum(pvred2<0.05)/m,3)
+
+# Test 3
+pvred3 <- replicate(m,testiraj(get.data(n, mu1, mu2, ro, b1, b2),
+                               perm.cols=c("X1","X2"), m.type="razmerje", n=N)$p)
+moc3 <- round(sum(pvred3<0.05)/m,3)
+
+# Test 4
+pvred4 <- replicate(m,testiraj(get.data(n, mu1, mu2, ro, b1, b2),
+                               perm.cols=c("y"), m.type="razmerje", n=N)$p)
+moc4 <- round(sum(pvred4<0.05)/m,3)
+
+moci.kor09 <- data.frame("Moc testa" = c(moc1,moc2,moc3,moc4),
+                        row.names = paste0("Test ",1:4))
+
+
+dump(c("podatki","test1.kor09","test2.kor09","test3.kor09","test4.kor09",
+       "velikosti.kor09","moci.kor09"), "rezultati_kor0.9.R")
+
